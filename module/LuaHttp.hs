fbbf291d7e9fdf228f935815635488adcb9e7c6a
Shubham Awasthi <aw.shubh@gmail.com>
2020-03-12 16:43:15 +0530

    LuaHttp: Send https request

--- module/LuaHttp.hs
+++ module/LuaHttp.hs
@@ -4,13 +4,15 @@ module LuaHttp where
     import Foreign.Lua as Lua
     import qualified Data.ByteString.Char8 as C8
     import qualified Data.ByteString.Lazy.Char8 as L8
-    import           Network.HTTP.Simple
+    import           Network.HTTP.Client
+    import           Network.HTTP.Client.TLS
 
     luaHttp :: Lua NumResults
     luaHttp = do
+        manager <- liftIO $ newManager tlsManagerSettings
         i <- peek 1 :: Lua String
         liftIO $ putStrLn $ "Sending Request to: "++ i
         initialRequest <- parseRequest i
-        response <- httpLBS initialRequest
-        pushstring $ C8.pack $ L8.unpack $ getResponseBody response
+        response <- liftIO $ httpLbs initialRequest manager  
+        pushstring $ C8.pack $ L8.unpack $ responseBody response
         return 1
